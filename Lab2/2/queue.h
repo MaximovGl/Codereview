5a7debd20bd43114c541144e4d41b549b42d1aed
MaximovGl <146012522+MaximovGl@users.noreply.github.com>
2025-08-19 17:23:04 +0500

    Update queue.h

--- Lab2/2/queue.h
+++ Lab2/2/queue.h
@@ -1,21 +1,25 @@
 #ifndef QUEUE_H
 #define QUEUE_H
-
 #include <iostream>
+using namespace std;
 
 class Node {
-public:
+private:
     int data;
     Node* next;
+public:
+    int getData() const { return data; }
+    void setData(int newData) { data = newData; }
+    Node* getNext() const { return next; }
+    void setNext(Node* newNext) { next = newNext; }
 };
 
 typedef Node* PNode;
 
-void Add(int data, PNode& Head, PNode& Tail);
-int Show_Before(PNode head);  
-int Delete(int m, PNode& Head);  
-void Show_After(PNode head);
-int Read_int();
-int RunQueueOperations(); 
+void add_node(int data, PNode& head, PNode& tail);
+void show_initial_queue(PNode head);
+void delete_n_elements(int n, PNode& head);
+void show_new_queue(PNode head);
+PNode input(int& n);
 
 #endif
