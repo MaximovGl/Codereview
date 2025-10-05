e2e1020c04fd8718ad44cdfba1915e5f93777da9
MaximovGl <146012522+MaximovGl@users.noreply.github.com>
2025-10-05 21:09:35 +0500

    Update word_shuffler.h

--- Lab3/1/word_shuffler.h
+++ Lab3/1/word_shuffler.h
@@ -11,7 +11,7 @@
   * @brief Перемешивает буквы в слове до получения исходного слова
   * @param word Исходное слово для перемешивания
   * @return int Количество попыток перемешивания до совпадения с исходным словом
-  * @throw std::invalid_argument Если передана пустая строка
   */
 
 int shuffle_until_original(const std::string& word);
+
