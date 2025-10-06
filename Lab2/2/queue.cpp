3b5e22077b2164dc4f41a4d68e55447ea801ea29
MaximovGl <146012522+MaximovGl@users.noreply.github.com>
2025-10-06 16:30:40 +0500

    Update queue.cpp

--- Lab2/2/queue.cpp
+++ Lab2/2/queue.cpp
@@ -1,105 +1,133 @@
 #include "queue.h"
 
-void add_node(int data, PNode& head, PNode& tail) {
-    PNode temporary_node;
-    if (head == NULL) {
-        head = new Node;
-        tail = head;
-        head->setNext(NULL);
-    }
-    else {
-        temporary_node = new Node;
-        tail->setNext(temporary_node);
-        tail = temporary_node;
-        tail->setNext(NULL);
-    }
-    tail->setData(data);
-}
+Queue::Queue() : front_node(nullptr), back_node(nullptr), element_count(0) {}
 
-void show_initial_queue(PNode head) {
-    PNode temporary_node = head;
-    cout << endl << "Исходный адрес первого элемента: " << head << " " << head->getData();
-    cout << endl << "Исходная очередь: ";
-    while (temporary_node != nullptr) {
-        cout << temporary_node->getData() << " ";
-        temporary_node = temporary_node->getNext();
-    }
-    PNode last = head;
-    while (last->getNext() != nullptr) {
-        last = last->getNext();
-    }
-    cout << endl << "Исходный адрес последнего элемента: " << last << " " << last->getData() << endl;
-}
-
-void delete_n_elements(int n, PNode& head) {
-    PNode temporary_node;
-    for (int i = 0; i < n; i++) {
-        if (head == nullptr)
-            break;
-        temporary_node = head;
-        cout << temporary_node->getData() << " " << temporary_node->getNext() << " ";
-        head = head->getNext();
+Queue::~Queue() {
+    while (front_node != nullptr) {
+        Node* temporary_node = front_node;
+        front_node = front_node->next;
         delete temporary_node;
-        cout << endl;
     }
-    cout << endl;
 }
 
-void show_new_queue(PNode head) {
-    PNode temporary_node = nullptr;
-    PNode temp2 = head;
-    if (head == nullptr) {
-        cout << "Очередь после удаления элементов оказалась пустой" << endl;
-        int* pointer = nullptr;
-        cout << "Начало очереди: " << pointer << endl;
-        cout << "Конец очереди: " << pointer << endl;
+void Queue::enqueue(int value) {
+    Node* new_node = new Node(value);
+    if (back_node == nullptr) {
+        front_node = new_node;
+        back_node = new_node;
+    } else {
+        back_node->next = new_node;
+        back_node = new_node;
     }
-    else {
-        cout << "Новое начало очереди: " << temp2 << " " << temp2->getData() << endl;
-        cout << "Оставшиеся элементы очереди: ";
-        while (head != nullptr) {
-            temporary_node = head;
-            cout << temporary_node->getData() << " ";
-            head = head->getNext();
+    element_count++;
+}
+
+void Queue::dequeue() {
+    if (front_node == nullptr) return;
+    
+    Node* temporary_node = front_node;
+    front_node = front_node->next;
+    if (front_node == nullptr) {
+        back_node = nullptr;
+    }
+    delete temporary_node;
+    element_count--;
+}
+
+bool Queue::is_empty() const {
+    return front_node == nullptr;
+}
+
+int Queue::get_front() const {
+    return front_node->data;
+}
+
+int Queue::get_size() const {
+    return element_count;
+}
+
+void Queue::display_initial() const {
+    Node* current_node = front_node;
+    std::cout << std::endl << "Исходный адрес первого элемента: " << front_node << " " << front_node->data;
+    std::cout << std::endl << "Исходная очередь: ";
+    while (current_node != nullptr) {
+        std::cout << current_node->data << " ";
+        current_node = current_node->next;
+    }
+    std::cout << std::endl << "Исходный адрес последнего элемента: " << back_node << " " << back_node->data << std::endl;
+}
+
+void Queue::remove_first_n_elements(int n) {
+    for (int i = 0; i < n; i++) {
+        if (front_node == nullptr) break;
+        Node* temporary_node = front_node;
+        std::cout << temporary_node->data << " " << temporary_node->next << " ";
+        front_node = front_node->next;
+        delete temporary_node;
+        element_count--;
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void Queue::display_after_removal() const {
+    if (front_node == nullptr) {
+        std::cout << "Очередь после удаления элементов оказалась пустой" << std::endl;
+        std::cout << "Начало очереди: " << nullptr << std::endl;
+        std::cout << "Конец очереди: " << nullptr << std::endl;
+    } else {
+        std::cout << "Новое начало очереди: " << front_node << " " << front_node->data << std::endl;
+        std::cout << "Оставшиеся элементы очереди: ";
+        Node* current_node = front_node;
+        Node* last_node = nullptr;
+        while (current_node != nullptr) {
+            last_node = current_node;
+            std::cout << current_node->data << " ";
+            current_node = current_node->next;
         }
-        cout << endl << "Новый конец очереди: " << temporary_node << " " << temporary_node->getData() << endl;
+        std::cout << std::endl << "Новый конец очереди: " << last_node << " " << last_node->data << std::endl;
     }
 }
 
-PNode input(int& n) {
-    int m;
-    PNode p1 = nullptr;
-    PNode p2 = nullptr;
-    cout << "Добро пожаловать!" << endl;
-    cout << "Перед нами новая задача:" << endl;
-    cout << "Дано число N (> 0) и указатели P1 и P2 на начало и конец непустой очереди." << endl;
-    cout << "Извлечь из очереди N начальных элементов" << endl;
-    cout << "---------------------------------------------------------" << endl;
-    cout << "Вам необходимо ввести количество элементов очереди и их значения через пробел " << endl;
-    cout << "Мы выведем новые адреса начала и конца очереди, извлечем n значений и выведем на экран" << endl;
-    cout << endl << endl << "----------------------------------------------------------------------------" << endl;
-    cout << "Введите число m - количество элементов очереди: ";
-    cin >> m;
-    if (cin.fail() || m <= 0) {
-        cerr << "Ошибка ввода: m должно быть положительным целым числом." << endl;
+Queue create_queue_from_input(int& elements_to_remove) {
+    int queue_size;
+    Queue queue;
+    
+    std::cout << "Добро пожаловать!" << std::endl;
+    std::cout << "Перед нами новая задача:" << std::endl;
+    std::cout << "Дано число N (> 0) и указатели P1 и P2 на начало и конец непустой очереди." << std::endl;
+    std::cout << "Извлечь из очереди N начальных элементов" << std::endl;
+    std::cout << "---------------------------------------------------------" << std::endl;
+    std::cout << "Вам необходимо ввести количество элементов очереди и их значения через пробел " << std::endl;
+    std::cout << "Мы выведем новые адреса начала и конца очереди, извлечем n значений и выведем на экран" << std::endl;
+    std::cout << std::endl << std::endl << "----------------------------------------------------------------------------" << std::endl;
+    std::cout << "Введите число m - количество элементов очереди: ";
+    std::cin >> queue_size;
+    
+    if (std::cin.fail() || queue_size <= 0) {
+        std::cerr << "Ошибка ввода: m должно быть положительным целым числом." << std::endl;
         exit(1);
     }
-    cout << endl << "Введите " << m << " значений элементов очереди через пробел ";
-    for (int i = 0; i < m; i++) {
-        int j;
-        cin >> j;
-        if (cin.fail()) {
-            cerr << "Ошибка ввода: элемент очереди должен быть целым числом." << endl;
+    
+    std::cout << std::endl << "Введите " << queue_size << " значений элементов очереди через пробел ";
+    for (int i = 0; i < queue_size; i++) {
+        int value;
+        std::cin >> value;
+        if (std::cin.fail()) {
+            std::cerr << "Ошибка ввода: элемент очереди должен быть целым числом." << std::endl;
             exit(1);
         }
-        add_node(j, p1, p2);
+        queue.enqueue(value);
     }
-    cout << endl << endl << "Введите число n(n>0) - количество элементов" << endl;
-    cout << "очереди, которые нужно извлечь из очереди: ";
-    cin >> n;
-    if (cin.fail() || n <= 0) {
-        cerr << "Ошибка ввода: n должно быть положительным целым числом." << endl;
+    
+    std::cout << std::endl << std::endl << "Введите число n(n>0) - количество элементов" << std::endl;
+    std::cout << "очереди, которые нужно извлечь из очереди: ";
+    std::cin >> elements_to_remove;
+    
+    if (std::cin.fail() || elements_to_remove <= 0) {
+        std::cerr << "Ошибка ввода: n должно быть положительным целым числом." << std::endl;
         exit(1);
     }
-    return p1;
+    
+    return queue;
 }
