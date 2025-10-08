7ab33d5d5993800a97492c61af4312be7fa0dc27
MaximovGl <146012522+MaximovGl@users.noreply.github.com>
2025-10-08 19:42:29 +0500

    Update Count.cpp

--- Lab1/task3/Count.cpp
+++ Lab1/task3/Count.cpp
@@ -27,22 +27,51 @@ void CalculateNumbers(int basis, int dig_in_num) {
     //FIXME Программа не работает даже на базовых тестах.
     //FIXME не было проверки ввода.
 
+    /*vector<vector<double>> dp(N + 1, vector<double>(K, 0));
+
+    for (int j = 0; j < K; ++j) {
+        dp[1][j] = 1;
+    }
+
+    for (int i = 2; i <= N; ++i) {
+        for (int j = 1; j < K; ++j) {
+            dp[i][0] += dp[i - 1][j];
+        }
+
+        for (int j = 1; j < K; ++j) {
+            for (int l = 0; l < K; ++l) {
+                dp[i][j] += dp[i - 1][l];
+            }
+        }
+    }
+
+    double result = 0;
+    for (int j = 1; j < K; ++j) {
+        result += dp[N][j];
+    }
+
+    cout << "Ответ: " << result << endl;
+
+
+    return 0;*/
+
+    //FIXME однобуквенные переменные
+    //int K, N;
+
+    //FIXME однобуквенные переменные
+    //vector<vector<double>> dp(dig_in_num + 1, vector<double>(basis, 0));
     vector<vector<double>> nums_w_last_dig(dig_in_num + 1, 
         vector<double>(basis, 0));
 
-    // Базовый случай: для чисел длины 1
     for (int digit = 0; digit < basis; ++digit) {
         nums_w_last_dig[1][digit] = 1;
     }
 
-    // Для чисел длины 2 и более
     for (int length = 2; length <= dig_in_num; ++length) {
-        // Для цифры 0: можно ставить только после ненулевых цифр
         for (int prev_digit = 1; prev_digit < basis; ++prev_digit) {
             nums_w_last_dig[length][0] += nums_w_last_dig[length - 1][prev_digit];
         }
         
-        // Для ненулевых цифр: можно ставить после любой цифры
         for (int digit = 1; digit < basis; ++digit) {
             for (int prev_digit = 0; prev_digit < basis; ++prev_digit) {
                 nums_w_last_dig[length][digit] += nums_w_last_dig[length - 1][prev_digit];
