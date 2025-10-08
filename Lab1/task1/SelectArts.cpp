489b813742ac9b134228f5b67dd1c490f400b660
MaximovGl <146012522+MaximovGl@users.noreply.github.com>
2025-10-08 14:50:18 +0500

    Update SelectArts.cpp

--- Lab1/task1/SelectArts.cpp
+++ Lab1/task1/SelectArts.cpp
@@ -7,6 +7,10 @@
 using namespace std;
 
 void SelectArts(vector<vector<int>>& weight_tax, int curr_art, int curr_weight, vector<int>& weights, vector<int>& taxes) {
+    /*FIXME скобки
+    if (curr_art == 0 || curr_weight == 0)
+        return;
+    */
     if (curr_art == 0 || curr_weight == 0) {
         return;
     }
@@ -25,14 +29,24 @@ void CalculateArts(int art_amount, int required_weight, vector<int>& weights, ve
         max_weight += w;
     }
 
+
+    /* FIXME название переменных + максимальное значение int
+    vector<vector<int>> dp(art_amount + 1, vector<int>(max_weight + 1, 999));
+    for (int i = 0; i <= art_amount; i++) {
+        dp[i][0] = 0;
+    }
+    */
+
     vector<vector<int>> weight_tax(art_amount + 1, vector<int>(max_weight + 1, 2147483647));
     for (int i = 0; i <= art_amount; ++i) {
         weight_tax[i][0] = 0;
     }
-
-    for (int curr_art = 1; curr_art <= art_amount; curr_art++) {
-        for (int curr_weight = 1; curr_weight <= max_weight; curr_weight++) {
-            if (curr_weight >= weights[curr_art] && weight_tax[curr_art - 1][curr_weight - weights[curr_art]] != 2147483647) {
+    /*FIXME непонятная переменная
+    for (int s = 1; s <= max_weight; s++) {
+    */
+    for (int curr_art = 1; curr_art <= art_amount; curr_art++) {  //проходим по всем  артефактам
+        for (int curr_weight = 1; curr_weight <= max_weight; curr_weight++) { //Проходим по всем весам от 1 до max.
+            if (curr_weight >= weights[curr_art] && weight_tax[curr_art - 1][curr_weight - weights[curr_art]] != 2147483647) { 
                 weight_tax[curr_art][curr_weight] = min(weight_tax[curr_art - 1][curr_weight], weight_tax[curr_art - 1][curr_weight - weights[curr_art]] + taxes[curr_art]);
             }
             else {
